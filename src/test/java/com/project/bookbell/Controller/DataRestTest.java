bd8804778555eb78ca9e2d04fcc0194d7366522e
yujin <kkkjj5199@naver.com>
2024-01-25 17:52:51 +0900

    spring data rest + hal 추가

---
+++ src/test/java/com/project/bookbell/Controller/DataRestTest.java
@@ -0,0 +1,57 @@
+package com.project.bookbell.Controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@WebMvcTest
+@DisplayName("Data Res Api 테스트")
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest
+public class DataRestTest {
+
+    private final MockMvc mvc;
+
+    public DataRestTest(@Autowired  MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("book 리스트 조회 api")
+    @Test
+     void givenNothing_whenRequestingBookList_then() throws Exception {
+        //given
+
+        //when
+        mvc.perform(get("/api/bookses"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+
+        //then
+
+    }
+    @DisplayName("book 리스트 단건 조회 api")
+    @Test
+     void givenNothing_whenRequestingBook_then() throws Exception {
+        //given
+
+        //when
+        mvc.perform(get("/api/bookses/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+        //then
+
+    }
+}
